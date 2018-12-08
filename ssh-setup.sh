f53c86384c7810abdfb306b4ad1ec020ed38fa07
hvloc15 <32784670+hvloc15@users.noreply.github.com>
2018-12-08 14:57:26 +0700

    Update ssh-setup.sh

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -23,7 +23,7 @@ echo "service started"
 #download ngrok 
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
-./ngrok authtoken 3mnGUgXZDkm5jUzU8K1B6_7AMQ72jQjbSyKjCZFMk1L
+./ngrok authtoken 84ZNbs1KpCTfJmgwioVDg_3rS6P5hskRMmT817Cq7Ps
 echo "ngrok stated"
 
 #install libs.
